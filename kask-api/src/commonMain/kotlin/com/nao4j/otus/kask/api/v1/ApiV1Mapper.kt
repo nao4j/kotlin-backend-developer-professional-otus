9209575be81b10a4b9e691c4fc6d2e326803af7e
nao4j <yegor.pilipenko@nao4j.com>
2023-08-06 19:24:51 +0400

    M2L4 DTO v1

---
+++ kask-api/src/commonMain/kotlin/com/nao4j/otus/kask/api/v1/ApiV1Mapper.kt
@@ -0,0 +1,52 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package com.nao4j.otus.kask.api.v1
+
+import com.nao4j.otus.kask.api.v1.models.IRequest
+import com.nao4j.otus.kask.api.v1.models.IResponse
+import com.nao4j.otus.kask.api.v1.models.QuestionCreateRequest
+import com.nao4j.otus.kask.api.v1.models.QuestionCreateResponse
+import com.nao4j.otus.kask.api.v1.models.QuestionDeleteRequest
+import com.nao4j.otus.kask.api.v1.models.QuestionDeleteResponse
+import com.nao4j.otus.kask.api.v1.models.QuestionReadRequest
+import com.nao4j.otus.kask.api.v1.models.QuestionReadResponse
+import com.nao4j.otus.kask.api.v1.models.QuestionUpdateRequest
+import com.nao4j.otus.kask.api.v1.models.QuestionUpdateResponse
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+
+internal val apiV2Mapper = Json {
+    classDiscriminator = "_"
+    encodeDefaults = true
+    ignoreUnknownKeys = true
+
+    serializersModule = SerializersModule {
+        setupPolymorphic()
+    }
+}
+
+internal val infos = listOf(
+    info(QuestionCreateRequest::class, IRequest::class, "create") { copy(requestType = it) },
+    info(QuestionReadRequest::class, IRequest::class, "read") { copy(requestType = it) },
+    info(QuestionUpdateRequest::class, IRequest::class, "update") { copy(requestType = it) },
+    info(QuestionDeleteRequest::class, IRequest::class, "delete") { copy(requestType = it) },
+
+    info(QuestionCreateResponse::class, IResponse::class, "create") { copy(responseType = it) },
+    info(QuestionReadResponse::class, IResponse::class, "read") { copy(responseType = it) },
+    info(QuestionUpdateResponse::class, IResponse::class, "update") { copy(responseType = it) },
+    info(QuestionDeleteResponse::class, IResponse::class, "delete") { copy(responseType = it) },
+)
+
+fun apiV1RequestSerialize(request: IRequest): String =
+    apiV2Mapper.encodeToString(request)
+
+fun <T : IRequest> apiV1RequestDeserialize(json: String): T =
+    apiV2Mapper.decodeFromString<IRequest>(json) as T
+
+fun apiV1ResponseSerialize(response: IResponse): String =
+    apiV2Mapper.encodeToString(response)
+
+fun <T : IResponse> apiV1ResponseDeserialize(json: String): T =
+    apiV2Mapper.decodeFromString<IResponse>(json) as T
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,15 @@ rootProject.name = "kotlin-backend-developer-professional-otus"
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val openapiVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion apply false
+        kotlin("plugin.serialization") version kotlinVersion apply false
+
+        id("org.openapi.generator") version openapiVersion apply false
     }
 }
 
 include("m1l1-hello")
+include("kask-api")
